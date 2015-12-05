bb8a36274823655d9522959bf3fed52b2e6f5347
Robert Youngman <rob_youngman@yahoo.com>
2015-12-05 22:09:02 +0000

    add, delete, toggle complete

--- js/toDoController.js
+++ js/toDoController.js
@@ -1,16 +1,59 @@
 toDo.controller('ToDoController', [function() {
   var self = this;
 
-  self.toDoList = {
-    "tasks": [
-      {
+  self.newTask  = '';
+  self.filter   = null;
+  self.toDoList = [
+     {
         "description": "Complete the challenge",
-        "completed": false,
+        "isCompleted": true,
       },
       {
         "description": "Install OS X El Capitan",
-        "completed": false,
+        "isCompleted": false,
+      },
+      {
+        "description": "Read Angular docs",
+        "isCompleted": false,
       }
-    ]
+    ];
+
+  self.doAddTask = function() {
+    console.log("hello doAddTask");
+    var task = {
+      description: self.newTask,
+      isCompleted: false
+    }
+    self.toDoList.push(task)
   };
+
+  self.doDeleteTask = function(index) {
+    console.log("hello doDeleteTask");
+    self.toDoList.splice(index,1);
+  };
+
+  self.doToggleCompleted = function(task) {
+    var completed = task.isCompleted;
+    console.log("hello doToggleCompleted");
+    console.log(task.isCompleted ? "Completed":"Not Completed");
+    // task.isCompleted = !completed;
+    // console.log(task.isCompleted ? "Completed":"Not Completed");
+  };
+
+  self.doShowAllTasks = function() {
+    console.log("hello doShowAllTasks");
+  };
+
+  self.doShowActiveTasks = function() {
+    console.log("hello doShowActiveTasks");
+  };
+
+  self.doShowCompletedTasks = function() {
+    console.log("hello doShowCompletedTasks");
+  };
+
+  self.doClearCompletedTasks = function() {
+    console.log("hello doClearCompletedTasks");
+  };
+
 }]);
--- test/toDoController.spec.js
+++ test/toDoController.spec.js
@@ -9,22 +9,75 @@ describe('ToDoController', function() {
 
   it('initializes with an empty list of tasks', function() {
     expect(ctrl.toDoList).toBeUndefined();
+    expect(ctrl.newTask).toBeUndefined();
   });
 
   describe('when displaying toDo list', function() {
-    var tasks = [
+    var list = [
       {
         "description": "Complete the challenge",
-        "completed": false,
+        "isCompleted": true,
       },
       {
         "description": "Install OS X El Capitan",
-        "completed": false,
+        "isCompleted": false,
+      },
+      {
+        "description": "Read Angular docs",
+        "isCompleted": false,
       }
     ];
 
-    it('display tasks', function() {
-        expect(ctrl.toDoList.tasks).toEqual(tasks);
+    it('displays all tasks', function() {
+      expect(ctrl.toDoList).toEqual(list);
     });
   });
+
+  describe('when adding a new task', function() {
+    var list = [
+      {
+        "description": "Complete the challenge",
+        "isCompleted": true,
+      },
+      {
+        "description": "Install OS X El Capitan",
+        "isCompleted": false,
+      },
+      {
+        "description": "Read Angular docs",
+        "isCompleted": false,
+      },
+      {
+        "description": "Read Node docs",
+        "isCompleted": false,
+      }
+    ];
+
+    it('add a new task', function() {
+      ctrl.newTask = "Read Node docs";
+      ctrl.doAddTask();
+      expect(ctrl.toDoList).toEqual(list);
+    });
+  });
+  describe('when deleting a new task', function() {
+
+    var list = [
+      {
+        "description": "Complete the challenge",
+        "isCompleted": true,
+      },
+      {
+        "description": "Read Angular docs",
+        "isCompleted": false,
+      }
+    ];
+
+    var index = 1;
+
+    it('deletes a task', function() {
+      ctrl.doDeleteTask(index);
+      expect(ctrl.toDoList).toEqual(list);
+    });
+  });
+
 });
